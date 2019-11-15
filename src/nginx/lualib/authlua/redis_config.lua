a5b8b8fd3076555626a376c4dc9594988cbddb25
linjiewei <linjiewei@szy.cn>
2019-11-15 14:48:35 +0800

    .

--- src/nginx/lualib/authlua/redis_config.lua
+++ src/nginx/lualib/authlua/redis_config.lua
@@ -1,12 +1,12 @@
 -- reids连接配置
-local ip = "r-bp143d381a65f5c4.redis.rds.aliyuncs.com";  --连接IP
-local pwd = "Szy123456";    --密码
+local ip = "";  --连接IP
+local pwd = "";    --密码
 local port = 6379;
 local timeout = 1;    --超时时间，单位秒
 
 
-local session_ip = "r-bp14ea6d4710e314.redis.rds.aliyuncs.com";
-local session_pwd = "r-bp14ea6d4710e314:3QDgKu327YQz";
+local session_ip = "";
+local session_pwd = "";
 local session_port = 6379;
 
 
